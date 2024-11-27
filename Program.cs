55ab6caecc21b2212954e3c67666029c580f02eb
majsan8 <maja.lifh01@gmail.com>
2024-11-27 10:44:34 +0100

    1klarkanske

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace par_4_labb_2
             Console.WriteLine("Välkommen till windchill beräknaren!");
             Console.WriteLine("Skriv in med siffror hur många grader celsius du har hos dig)");
             string userInput1 = Console.ReadLine();
-            Console.WriteLine("Skriv i siffror hur snabbt vinden blåser (i km/h)");
+            Console.WriteLine("Skriv också i siffror hur snabbt vinden blåser (i km/h)");
 
 
             string userInput2 = Console.ReadLine();
@@ -22,7 +22,7 @@ namespace par_4_labb_2
 
             double WCTCelsius​ = 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(basenumber, exponent) + 0.3965 * temperature * Math.Pow(basenumber, exponent);
             string realcold = string.Format("{0:F2}", WCTCelsius);
-            Console.WriteLine(realcold); 
+            Console.WriteLine("Vädret utomhus känns just nu som:" + " " + realcold + " " + "C°"); 
         } 
                 
         
